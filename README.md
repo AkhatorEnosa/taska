372049b76efc1d004d3c3a3d613d79f3539dd7f7
AkhatorEnosa <aosakhogba@gmail.com>
2025-03-24 05:22:07 +0100

    initial commit for a Task Management Web App

---
+++ README.md
@@ -0,0 +1,3 @@
+# Task Management Web App
+
+A simple web application to help you manage your tasks effectively.
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-gray-200/20 px-20 py-20">
+      <h2 className="text-3xl font-semibold tracking-tight">This is going to be Task Management Web App</h2>
+    </div>
+  )
+}
+
+export default App
